ff03939e0c46b7e9fd5f0233f0bb0a81dfb33b0c
Dharmendra Rawat <dharmendrarawat055@gmail.com>
2024-02-09 08:43:24 +0530

    app updated

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,19 @@ const url = process.env.DBURL; //import url from dotenv
 const path = require("path")
 app.use(express.json()) //  to read data as json from frontend
 // cors for local server
-app.use(cors())
-// app.use(cors({
-//     origin: [
-//         "https://euphonious-cendol-e73ba8.netlify.app",
-//         "https://65c53304c4c9ae0c55ab378a--euphonious-cendol-e73ba8.netlify.app",
-//         "http://localhost:3001"
-//     ],
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true,
-// }));
+// app.use(cors())
+app.use(cors({
+    origin: [
+        "https://65c5914e96caa7486f2287a2--sweet-cupcake-a0d729.netlify.app",
+        "https://euphonious-cendol-e73ba8.netlify.app",
+        "http://localhost:3001",
+        "http://localhost:3000",
+        // Add other allowed origins as needed
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    exposedHeaders: ["Content-Type"] // Specify the headers you want to expose
+}));
 
 //  connect database
 mongoose.connect(url).then(() => {
