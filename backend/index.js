cc792e2e61570b15a54639708f90168eac0a08e6
Dharmendra Rawat <dharmendrarawat055@gmail.com>
2024-02-09 08:10:00 +0530

    app updated

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,16 @@ const url = process.env.DBURL; //import url from dotenv
 const path = require("path")
 app.use(express.json()) //  to read data as json from frontend
 // cors for local server
-// app.use(cors())
-
-// cors for live server
-app.use(
-    cors({
-        origin: ["https://euphonious-cendol-e73ba8.netlify.app", "https://65c53304c4c9ae0c55ab378a--euphonious-cendol-e73ba8.netlify.app", "http://localhost:3001"], // Replace with your React frontend's URL
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials: true,
-    })
-);
+app.use(cors())
+// app.use(cors({
+//     origin: [
+//         "https://euphonious-cendol-e73ba8.netlify.app",
+//         "https://65c53304c4c9ae0c55ab378a--euphonious-cendol-e73ba8.netlify.app",
+//         "http://localhost:3001"
+//     ],
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+// }));
 
 //  connect database
 mongoose.connect(url).then(() => {
@@ -27,8 +27,6 @@ mongoose.connect(url).then(() => {
 }).catch((err) => {
     console.log(err);
 })
-
-app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(route)  //user routes
 app.listen(PORT, () => {
     console.log(`server is runing on port ${PORT}`);
